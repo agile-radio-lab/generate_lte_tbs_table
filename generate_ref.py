54d3d00b48e2199ffd0a6a043fa1a57f4604b934
Igor Kim <igor.skh@gmail.com>
2019-08-14 19:13:00 +0200

    add parameters for length code formatting

--- generate_ref.py
+++ generate_ref.py
@@ -10,42 +10,40 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", default="samples/ts_136213v121200p.txt", type=str, help="Path to reference text file")
+parser.add_argument("-f", "--file", default="samples/ts_136213v121200p.txt",
+                    type=str, help="Path to reference text file")
+parser.add_argument("-n", "--header", default=10,
+                    type=int, help="Header line length")
+parser.add_argument("-c", "--content", default=11,
+                    type=int, help="Content line length")
 args = parser.parse_args()
 
 fname = args.file
 fname_base = os.path.basename(os.path.splitext(fname)[0])
 f = open(fname)
-lines = f.readlines()
-
-LEN_HEADER_IDX_LEN = 10  
-LEN_CONTENT = 11
-EXPECTED_LEN = LEN_CONTENT*LEN_HEADER_IDX_LEN
 
 i_tbs_result = {}
-
-for line in lines:
+for line in f:
     elements = line.strip().split()
-    n_elements = len(elements) 
-
-    if n_elements == LEN_CONTENT:
+    n_elements = len(elements)
+    if n_elements == args.content:
         i_tbs = elements[0]
         tbs_list = list(map(int, elements[1:]))
         if not i_tbs in i_tbs_result:
             i_tbs_result[i_tbs] = []
         i_tbs_result[i_tbs].extend(tbs_list)
-    elif n_elements == LEN_HEADER_IDX_LEN:
+    elif n_elements == args.header:
         pass
 
 # check lengths
 all_correct = True
 for i_tbs in i_tbs_result:
-    if len(i_tbs_result[i_tbs]) != EXPECTED_LEN:
-        print("Wrong length on I_TBS = %s"%i_tbs)
+    if len(i_tbs_result[i_tbs]) != args.content*args.header:
+        print("Wrong length on I_TBS = %s" % i_tbs)
         all_correct = False
 if all_correct:
     print("Length checked: OK")
 
 if not os.path.exists("generated"):
     os.mkdir("generated")
-json.dump(i_tbs_result, open("generated/%s.json"%fname_base, "w"))
+json.dump(i_tbs_result, open("generated/%s.json" % fname_base, "w"))
