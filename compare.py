30c4c885c764da2a7461e3e83552e855883e38bf
Igor Kim <igor.skh@gmail.com>
2019-08-14 20:12:00 +0200

    implement functions pep8 linting

--- compare.py
+++ compare.py
@@ -1,3 +1,7 @@
+import json
+import os
+import argparse
+
 __author__ = "Igor Kim"
 __credits__ = ["Igor Kim"]
 __maintainer__ = "Igor Kim"
@@ -5,58 +9,75 @@ __email__ = "igor.skh@gmail.com"
 __date__ = "08/2019"
 __license__ = "MIT"
 
-import json
-import os
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", default="generated/ts_136213v121200p.json", type=str, help="Path to reference JSON file")
-parser.add_argument("-c", "--compare", default="samples/cpp_tbs.json", type=str, help="Path to target file")
-parser.add_argument("-o", "--output", default="generated/report.html", type=str, help="Path to report")
-args = parser.parse_args()
-
-fname_ref = args.file
-fname_comp = args.compare
-
-ref_table = json.load(open(fname_ref))
-comp_tbs_table = json.load(open(fname_comp))
 
 def to_html(arr, is_correct):
     res = "<tr>"
     for i, val in enumerate(arr):
         color = "black" if is_correct[i] else "red"
-        res += "<td style=\"color:%s\">%d</td>"%(color, val)
+        res += "<td style=\"color:%s\">%d</td>" % (color, val)
     res += "</tr>"
     return res
 
-report_html = "<table style=\"text-align:left;\">"
-errors_cnt = 0
-for i, tbs_list in enumerate(comp_tbs_table):
-    i_tbs = str(i)
-    if i_tbs in ref_table:
-        len_ref = len(ref_table[i_tbs])
-        len_comp = len(tbs_list)
-        if len_ref != len_comp:
-            report_html += "<tr><th colspan=\"%d\">I_TBS = %s, \
-                inconsistent length got %d, expected %d</th></tr>"\
-                    %(len_ref, i_tbs, len_comp, len_ref)
-            print("Inconsistent length, I_TBS = %s"%i_tbs)
-            errors_cnt += 1
-            continue
-    
-        compare = [ref_table[i_tbs][i] == tbs_list[i] for i in range(len_ref)]
-        if not all(compare):
-            report_html += "<tr><th colspan=\"%d\">I_TBS = %s</th></tr>"%(len_ref, i_tbs)
-            report_html += to_html(ref_table[i_tbs], compare)
-            report_html += to_html(tbs_list, compare)
-            print("Incorrect values found, I_TBS = %s"%i_tbs)
-            errors_cnt += 1
-report_html += "</table>"
 
-print("%d error(s) found"%errors_cnt)
-if errors_cnt > 0:
-    print("Writting report to: %s"%args.output)
-    if not os.path.exists("generated"):
-        os.mkdir("generated")
-    f = open(args.output, "w")
-    f.write(report_html)
+def check_values(ref_row, comp_row):
+    compare = [ref_row[i] == comp_row[i] for i in range(len(ref_row))]
+    return not all(compare), compare
+
+
+def check_line(i_tbs, ref_row, comp_row):
+    len_ref = len(ref_row)
+    len_comp = len(comp_row)
+    if len_ref != len_comp:
+        report_html = "<tr><th colspan=\"%d\">I_TBS = %s, \
+                inconsistent length got %d, expected %d</th></tr>"\
+                % (len_ref, i_tbs, len_comp, len_ref)
+        print("Inconsistent length, I_TBS = %s" % i_tbs)
+        return True, report_html
+
+    has_wrong, compare = check_values(ref_row, comp_row)
+    if has_wrong:
+        report_html = "<tr><th colspan=\"%d\">I_TBS = %s</th></tr>" % (
+            len_ref, i_tbs)
+        report_html += to_html(ref_row, compare)
+        report_html += to_html(comp_row, compare)
+        print("Incorrect values found, I_TBS = %s" % i_tbs)
+        return True, report_html
+    return False, None
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file",
+                        default="generated/ts_136213v121200p.json",
+                        type=str, help="Path to reference JSON file")
+    parser.add_argument("-c", "--compare", default="samples/cpp_tbs.json",
+                        type=str, help="Path to target file")
+    parser.add_argument("-o", "--output", default="generated/report.html",
+                        type=str, help="Path to report")
+    args = parser.parse_args()
+
+    fname_ref = args.file
+    fname_comp = args.compare
+
+    ref_table = json.load(open(fname_ref))
+    comp_tbs_table = json.load(open(fname_comp))
+
+    report_html = "<table style=\"text-align:left;\">"
+    errors_cnt = 0
+    for i, tbs_list in enumerate(comp_tbs_table):
+        i_tbs = str(i)
+        if i_tbs not in ref_table:
+            continue
+        has_error, report_part = check_line(str(i), ref_table[i_tbs], tbs_list)
+        if has_error:
+            errors_cnt += 1
+            report_html += report_part
+    report_html += "</table>"
+
+    print("%d error(s) found" % errors_cnt)
+    if errors_cnt > 0:
+        print("Writing report to: %s" % args.output)
+        if not os.path.exists("generated"):
+            os.mkdir("generated")
+        f = open(args.output, "w")
+        f.write(report_html)
--- generate_ref.py
+++ generate_ref.py
@@ -1,3 +1,7 @@
+import json
+import os
+import argparse
+
 __author__ = "Igor Kim"
 __credits__ = ["Igor Kim"]
 __maintainer__ = "Igor Kim"
@@ -5,45 +9,52 @@ __email__ = "igor.skh@gmail.com"
 __date__ = "08/2019"
 __license__ = "MIT"
 
-import json
-import os
-import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", default="samples/ts_136213v121200p.txt",
-                    type=str, help="Path to reference text file")
-parser.add_argument("-n", "--header", default=10,
-                    type=int, help="Header line length")
-parser.add_argument("-c", "--content", default=11,
-                    type=int, help="Content line length")
-args = parser.parse_args()
+def generate_tbs_ref_table(file_h, n_header, n_content):
+    i_tbs_result = {}
+    for line in file_h:
+        elements = line.strip().split()
+        n_elements = len(elements)
+        if n_elements == n_content:
+            i_tbs = elements[0]
+            tbs_list = list(map(int, elements[1:]))
+            if i_tbs not in i_tbs_result:
+                i_tbs_result[i_tbs] = []
+            i_tbs_result[i_tbs].extend(tbs_list)
+        elif n_elements == n_header:
+            # do something with the n_prbs
+            pass
+    return i_tbs_result
 
-fname = args.file
-fname_base = os.path.basename(os.path.splitext(fname)[0])
-f = open(fname)
 
-i_tbs_result = {}
-for line in f:
-    elements = line.strip().split()
-    n_elements = len(elements)
-    if n_elements == args.content:
-        i_tbs = elements[0]
-        tbs_list = list(map(int, elements[1:]))
-        if not i_tbs in i_tbs_result:
-            i_tbs_result[i_tbs] = []
-        i_tbs_result[i_tbs].extend(tbs_list)
-    elif n_elements == args.header:
-        pass
+def check_length(ref_table, n_total):
+    all_correct = True
+    for i_tbs in ref_table:
+        if len(ref_table[i_tbs]) != n_total:
+            print("Wrong length on I_TBS = %s" % i_tbs)
+            all_correct = False
+            break
+    return all_correct
 
-# check lengths
-all_correct = True
-for i_tbs in i_tbs_result:
-    if len(i_tbs_result[i_tbs]) != args.content*args.header:
-        print("Wrong length on I_TBS = %s" % i_tbs)
-        all_correct = False
-if all_correct:
-    print("Length checked: OK")
 
-if not os.path.exists("generated"):
-    os.mkdir("generated")
-json.dump(i_tbs_result, open("generated/%s.json" % fname_base, "w"))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file",
+                        default="samples/ts_136213v121200p.txt",
+                        type=str, help="Path to reference text file")
+    parser.add_argument("-n", "--header", default=10,
+                        type=int, help="Header line length")
+    parser.add_argument("-c", "--content", default=11,
+                        type=int, help="Content line length")
+    args = parser.parse_args()
+
+    fname = args.file
+    fname_base = os.path.basename(os.path.splitext(fname)[0])
+
+    ref_table = generate_tbs_ref_table(open(fname), args.header, args.content)
+    if check_length(ref_table, args.header*args.content):
+        print("Length checked: OK")
+
+    if not os.path.exists("generated"):
+        os.mkdir("generated")
+    json.dump(ref_table, open("generated/%s.json" % fname_base, "w"))
